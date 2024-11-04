702ba440b68e43989f5d2e5d34a59f7978a80003
Alixes <ixes@mail.ru>
2024-11-04 12:07:42 +0300

    fix btntranslate, change func

--- src/Components/ButtonTranslate/ButtonTranslate.jsx
+++ src/Components/ButtonTranslate/ButtonTranslate.jsx
@@ -4,11 +4,16 @@ import style from "./buttontranslate.module.scss";
 export default function ButtonTranslate({ handleClick, translate, pressed }) {
   return (
     <div onClick={handleClick} className={style.button}>
+      {pressed && <span className={style.translate}>книга{translate}</span>}
+      {!pressed && <button className={style.button}>Показать перевод</button>}
+    </div>
+  );
+}
+
+/*<div onClick={handleClick} className={style.button}>
       {pressed ? (
         <span className={style.translate}>книга{translate}</span>
       ) : (
         <button className={style.button}>Показать перевод</button>
       )}
-    </div>
-  );
-}
+    </div>*/
