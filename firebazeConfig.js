f8e9ee4f8561aa0762c3ef4f1ef6efb8cd204e1d
Alixes <ixes@mail.ru>
2024-12-09 12:51:41 +0300

    add firebase hosting

---
+++ firebazeConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMg0mEG2WFIZoVDrqUR8IE552jAVUIsGg",
+  authDomain: "react---learn-language.firebaseapp.com",
+  projectId: "react---learn-language",
+  storageBucket: "react---learn-language.firebasestorage.app",
+  messagingSenderId: "252261203837",
+  appId: "1:252261203837:web:56dbe0ffb43b5849975614",
+  measurementId: "G-3GBBCSMDRG",
+};
