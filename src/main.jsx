c5243512f4856e0f79bf702f6d571dbfacf06a18
Alixes <ixes@mail.ru>
2024-12-18 22:15:19 +0300

    add api fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,10 @@ import { MyContextProvider } from "./Context/MyContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MyContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MyContextProvider>
   </React.StrictMode>
 );
